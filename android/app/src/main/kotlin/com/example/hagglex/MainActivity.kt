926e26e4cc4f35827c8a32d014687cabe6f943a1
iamstanlee <akpamastanlee@gmail.com>
2021-04-25 16:59:55 -0700

    finished theme setup

--- android/app/src/main/kotlin/com/example/hagglex/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/hagglex/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.example.hagglex
 
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.android.SplashScreen
 
 class MainActivity: FlutterActivity() {
+
+    override fun provideSplashScreen(): SplashScreen? = SplashView()
 }
---
+++ lib/ui/signup/signup.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:hagglex/utils/helpers.dart';
+import 'package:hagglex/utils/res.dart';
+
+class Signup extends StatefulWidget {
+  @override
+  _SignupState createState() => _SignupState();
+}
+
+class _SignupState extends State<Signup> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: R.Colors.primaryColor,
+      // body: ,
+    );
+  }
+}
